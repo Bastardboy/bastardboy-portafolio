4b0b8ecf1c67dc2a0d787709214bb2b4938e6cd3
Bastardboy <david.pazan2@gmail.com>
2025-05-19 19:45:10 -0400

    Actualización portafiolo: Mejora visual Landscape principalmente Actualización iconos Mnimalismo en acciones Separaciones de activades

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,11 @@
-module.exports = {
+/** @type {import('tailwindcss').Config} */
+
+
+export default {
+    content: ['./src/**/*.{astro,html,js,jsx,ts,tsx,md,mdx,vue,svelte}'],
     darkMode: 'class',
+    theme: {
+        extend: {},
+    },
+    plugins: [],
 }
